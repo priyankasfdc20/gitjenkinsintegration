62fcd7c3c2915359c6c4d97982dff02be89c6cb7
priya <priyanka.gaddam@osi.ca.gov>
2020-06-22 15:16:37 -0700

    add priya.java in git

---
+++ priya.java
@@ -0,0 +1,12 @@
+class Priya
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, priya" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+		for(int i=0;i<10,i++)
+        {
+        System.out.println("jenkins and github integration test ,java priya file"); 
+         }
+    } 
+} 
